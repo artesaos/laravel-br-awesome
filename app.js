e86336b97fdac79b3d0120019ede6584c9a3e79d
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-09-08 23:07:35 -0300

    new layout

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 (function(){
 	angular.module('awesome', [])
+	.directive('widget', function() {
+	  return {
+	      restrict: 'C',
+	      link: function(scope, elem, attrs) {
+	      	$(elem).geopattern(scope.item.name, {
+	      		generator: 'squares'
+	      	});
+	      }
+	  };
+	})
 	.controller('filterFormController', ['$scope', '$http', function($scope, $http){
 		var init = function() {
 			$http.get('itens.json')
